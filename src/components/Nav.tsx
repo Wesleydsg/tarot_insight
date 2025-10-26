4bfb160792c35667ef68ff73025a9f6c974d69da
Diegovsky <diegovsky.dev@gmail.com>
2025-10-26 07:27:12 -0300

    Move to next.js

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,11 @@
-import { useLocation } from "@solidjs/router";
-
 export default function Nav() {
-  const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Home</a>
-        </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
-        </li>
+    <nav className="bg-zinc-800 px-4 items-center h-16 flex">
+      <span>Tarot Insight (logo)</span>
+      <div className="grow-1"></div>
+      <ul className="flex gap-2">
+        <li>Home</li>
+        <li>Login</li>
       </ul>
     </nav>
   );
